8ea011705284139503da2bbc58914dec4a893b94
ktny <katsunori.t10@gmail.com>
2021-03-07 23:26:54 +0900

    add a pivot table and chart

--- work/sheets.js
+++ work/sheets.js
@@ -21,6 +21,68 @@ var COLUMNS = [
   { field: 'status', header: 'Status'}
 ];
 
+SheetsHelper.prototype.createSpreadsheet = function(title, callback) {
+  var self = this;
+  var request = {
+    resource: {
+      properties: {
+        title: title
+      },
+      sheets: [
+        {
+          properties: {
+            title: 'Data',
+            gridProperties: {
+              columnCount: 6,
+              frozenRowCount: 1
+            }
+          }
+        },
+        {
+          properties: {
+            title: 'Pivot',
+            gridProperties: {
+              hideGridlines: true,
+            },
+          }
+        }
+      ]
+    }
+  };
+  // スプレッドシートを作成
+  self.service.spreadsheets.create(request, function(err, response) {
+    if (err) {
+      return callback(err);
+    }
+    var spreadsheet = response.data;
+    var dataSheetId = spreadsheet.sheets[0].properties.sheetId;
+    var requests = [
+      buildHeaderRowRequest(dataSheetId)
+    ];
+
+    var pivotSheetId = spreadsheet.sheets[1].properties.sheetId;
+    requests = requests.concat([
+      buildPivotTableRequest(dataSheetId, pivotSheetId),
+      buildFormatPivotTableRequest(pivotSheetId),
+      buildAddChartRequest(pivotSheetId),
+    ]);
+
+    var request = {
+      spreadsheetId: spreadsheet.spreadsheetId,
+      resource: {
+        requests: requests,
+      }
+    };
+    // 作成したスプレッドシートを更新
+    self.service.spreadsheets.batchUpdate(request, function(err, response) {
+      if (err) {
+        return callback(err);
+      }
+      return callback(null, spreadsheet);
+    });
+  });
+}
+
 function buildHeaderRowRequest(sheetId) {
   var cells = COLUMNS.map(function(column) {
     return {
@@ -51,50 +113,117 @@ function buildHeaderRowRequest(sheetId) {
   }
 }
 
-SheetsHelper.prototype.createSpreadsheet = function(title, callback) {
-  var self = this;
-  var request = {
-    resource: {
-      properties: {
-        title: title
-      },
-      sheets: [
+function buildPivotTableRequest(sourceSheetId, targetSheetId) {
+  return {
+    updateCells: {
+      start: { sheetId: targetSheetId, rowIndex: 0, columnIndex: 0 },
+      rows: [
         {
-          properties: {
-            title: 'Data',
-            gridProperties: {
-              columnCount: 6,
-              frozenRowCount: 1
+          values: [
+            {
+              pivotTable: {
+                source: {
+                  sheetId: sourceSheetId,
+                  startRowIndex: 0,
+                  startColumnIndex: 0,
+                  endColumnIndex: COLUMNS.length
+                },
+                rows: [
+                  {
+                    sourceColumnOffset: getColumnForField('productCode').index,
+                    showTotals: false,
+                    sortOrder: 'ASCENDING'
+                  }
+                ],
+                values: [
+                  {
+                    summarizeFunction: 'SUM',
+                    sourceColumnOffset: getColumnForField('unitsOrdered').index
+                  },
+                  {
+                    summarizeFunction: 'SUM',
+                    name: 'Revenue',
+                    formula: util.format("='%s' * '%s'",
+                        getColumnForField('unitsOrdered').header,
+                        getColumnForField('unitPrice').header)
+                  }
+                ]
+              }
             }
-          }
+          ]
         }
-      ]
+      ],
+      fields: '*'
     }
   };
-  // スプレッドシートを作成
-  self.service.spreadsheets.create(request, function(err, response) {
-    if (err) {
-      return callback(err);
-    }
-    var spreadsheet = response.data;
-    var dataSheetId = spreadsheet.sheets[0].properties.sheetId;
-    var requests = [
-      buildHeaderRowRequest(dataSheetId)
-    ];
+}
 
-    var request = {
-      spreadsheetId: spreadsheet.spreadsheetId,
-      resource: {
-        requests: requests,
+function buildFormatPivotTableRequest(sheetId) {
+  return {
+    repeatCell: {
+      range: { sheetId: sheetId, startRowIndex: 1, startColumnIndex: 2 },
+      cell: {
+        userEnteredFormat: {
+          numberFormat: { type: 'CURRENCY', pattern: '"$"#,##0.00' }
+        }
+      },
+      fields: 'userEnteredFormat.numberFormat'
+    }
+  };
+}
+
+function buildAddChartRequest(sheetId) {
+  return {
+    addChart: {
+      chart: {
+        spec: {
+          title: 'Revenue per Product',
+          basicChart: {
+            chartType: 'BAR',
+            legendPosition: 'RIGHT_LEGEND',
+            domains: [
+              // Show a bar for each product code in the pivot table.
+              {
+                domain: { sourceRange: { sources: [{
+                  sheetId: sheetId,
+                  startRowIndex: 0,
+                  startColumnIndex: 0,
+                  endColumnIndex: 1
+                }]}}
+              }
+            ],
+            series: [
+              // Set that bar's length based on the total revenue.
+              {
+                series: { sourceRange: { sources: [{
+                  sheetId: sheetId,
+                  startRowIndex: 0,
+                  startColumnIndex: 2,
+                  endColumnIndex: 3
+                }]}}
+              }
+            ]
+          }
+        },
+        position: {
+          overlayPosition: {
+            anchorCell: { sheetId: sheetId, rowIndex: 0, columnIndex: 3 },
+            widthPixels: 600,
+            heightPixels: 400
+          }
+        }
       }
-    };
-    // 作成したスプレッドシートを更新
-    self.service.spreadsheets.batchUpdate(request, function(err, response) {
-      if (err) {
-        return callback(err);
-      }
-      return callback(null, spreadsheet);
-    });
+    }
+  };
+}
+
+function getColumnForField(field) {
+  return COLUMNS.reduce(function(result, column, i) {
+    if (column.field == field) {
+      column.index = i;
+      return column;
+    }
+    return result;
   });
 }
 
