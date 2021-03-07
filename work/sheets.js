a04c51d08406b1c0a41c003988ad29cd208fe4d2
ktny <katsunori.t10@gmail.com>
2021-03-07 15:31:10 +0900

    add header row

--- work/sheets.js
+++ work/sheets.js
@@ -12,6 +12,45 @@ var SheetsHelper = function(accessToken) {
 
 module.exports = SheetsHelper;
 
+var COLUMNS = [
+  { field: 'id', header: 'ID' },
+  { field: 'customerName', header: 'Customer Name'},
+  { field: 'productCode', header: 'Product Code' },
+  { field: 'unitsOrdered', header: 'Units Ordered' },
+  { field: 'unitPrice', header: 'Unit Price' },
+  { field: 'status', header: 'Status'}
+];
+
+function buildHeaderRowRequest(sheetId) {
+  var cells = COLUMNS.map(function(column) {
+    return {
+      userEnteredValue: {
+        stringValue: column.header,
+      },
+      userEnteredFormat: {
+        textFormat: {
+          bold: true
+        }
+      }
+    }
+  });
+  return {
+    updateCells: {
+      start: {
+        sheetId: sheetId,
+        rowIndex: 0,
+        columnIndex: 0,
+      },
+      rows: [
+        {
+          values: cells,
+        }
+      ],
+      fields: 'userEnteredValue,userEnteredFormat.textFormat.bold',
+    }
+  }
+}
+
 SheetsHelper.prototype.createSpreadsheet = function(title, callback) {
   var self = this;
   var request = {
@@ -32,11 +71,29 @@ SheetsHelper.prototype.createSpreadsheet = function(title, callback) {
       ]
     }
   };
+  // スプレッドシートを作成
   self.service.spreadsheets.create(request, function(err, response) {
     if (err) {
       return callback(err);
     }
     var spreadsheet = response.data;
-    return callback(null, spreadsheet);
-  }) 
+    var dataSheetId = spreadsheet.sheets[0].properties.sheetId;
+    var requests = [
+      buildHeaderRowRequest(dataSheetId)
+    ];
+
+    var request = {
+      spreadsheetId: spreadsheet.spreadsheetId,
+      resource: {
+        requests: requests,
+      }
+    };
+    // 作成したスプレッドシートを更新
+    self.service.spreadsheets.batchUpdate(request, function(err, response) {
+      if (err) {
+        return callback(err);
+      }
+      return callback(null, spreadsheet);
+    });
+  });
 }
